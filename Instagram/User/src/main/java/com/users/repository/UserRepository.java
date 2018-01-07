cb0fc4e191fa1f7d05f1c2662ec15a7931dcb964
SabuShakya <sabushakya1@gmail.com>
2018-01-07 10:22:34 +0545

    changes

--- Instagram/User/src/main/java/com/users/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.users.repository;
-
-import com.users.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    public User getByUname(String uname);
-}
